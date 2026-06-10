eb1525765a2a16ef3491c2b89a654b631eb76e78
DanielShlein <danschlein@proton.me>
2026-06-10 12:04:57 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Judaica Ukraine</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/font.css">
+
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/loginmedia.css">
+    <script src="js/jquery.js"></script>
+
+
+    <script src="js/swiper.js"></script>
+</head>
+<body class="body">
+<div class="wrapper">
+    <div class="welcome__block">
+        <img src="images/Logo_l.svg" alt="" class="logo__main">
+        <div class="form__block">
+            <div class="input__block">
+                <p class="input__label">Телефон</p>
+                <input type="text" class="input__inner">
+            </div>
+            <div class="input__block">
+                <p class="input__label">Пароль</p>
+                <input type="password" class="input__inner">
+            </div>
+        </div>
+        <div class="button-info__block">
+            <a href="/" class="login__button">Увійти</a>
+            <p class="login__info">Немає облікового запису? <a href="/signup.php" class="newacc__info">Створити новий обліковий запис</a></p>
+        </div>
+        <div class="legal-info__block">
+            Авторизуючись на сайті Ви погоджуєтеся з <a href="/" class="politics__one">політикою конфіденційності</a> та <a href="/" class="politics__two">правилами сайту</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
